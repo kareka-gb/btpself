d9b1fb6e3a5ab1e340e28bbafed1d3ea3c4a54f6
kareka-gb <kanala_k@ec.iitr.ac.in>
2023-05-14 19:36:14 +0530

    Added Models, data generator, utils and Sat_rgb

---
+++ data.py
@@ -0,0 +1,55 @@
+import os
+import tensorflow as tf
+from tensorflow.python.data.experimental import AUTOTUNE
+
+class drone_data:
+    def __init__(self, subset='train', images_dir='data') -> None:
+        if subset == 'train':
+            self.image_ids = range(1, 1601)
+        elif subset == 'valid':
+            self.image_ids = range(1601, 1801)
+        elif subset == 'test':
+            self.image_ids = range(1801, 1806)
+        else:
+            raise ValueError("subset must be 'train' or 'valid'")
+        
+        self.images_dir = images_dir
+    
+    def __len__(self):
+        return len(self.image_ids)
+    
+    def dataset(self, batch_size=16, repeat_count=None):
+        ds = tf.data.Dataset.zip((self.lr_dataset(), self.hr_dataset()))
+        ds = ds.batch(batch_size)
+        ds = ds.repeat(repeat_count)
+        ds = ds.prefetch(buffer_size=AUTOTUNE)
+        return ds
+    
+    def hr_dataset(self):
+        ds = self._images_dataset(self._hr_image_files())
+        return ds
+
+    def lr_dataset(self):
+        ds = self._images_dataset(self._lr_image_files())
+        return ds
+
+    def _hr_image_files(self):
+        images_dir = self._hr_images_dir()
+        return [os.path.join(images_dir, f'{image_id}.png') for image_id in self.image_ids]
+
+    def _lr_image_files(self):
+        images_dir = self._lr_images_dir()
+        return [os.path.join(images_dir, f'{image_id}_04.png') for image_id in self.image_ids]
+
+    def _hr_images_dir(self):
+        return os.path.join(self.images_dir, f'drone_rgb_hr')
+
+    def _lr_images_dir(self):
+        return os.path.join(self.images_dir, f'drone_rgb_lr')
+    
+    @staticmethod
+    def _images_dataset(image_files):
+        ds = tf.data.Dataset.from_tensor_slices(image_files)
+        ds = ds.map(tf.io.read_file)
+        ds = ds.map(lambda x: tf.image.decode_png(x, channels=3), num_parallel_calls=AUTOTUNE)
+        return ds
---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+from model.common import resolve
+from model.common import resolve_single
+from model.common import evaluate
+
+# Taken from
+# https://github.com/krasserm/super-resolution/
+# Thank you for this wonderful resource
